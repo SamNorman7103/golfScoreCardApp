922db6993badf4aed1f694e2d8d244b22ac6fb78
SamNorman7103 <70028250+SamNorman7103@users.noreply.github.com>
2020-11-06 08:48:33 -0700

    created javascript functions to get data from the API and to populate the par, yards and handicap data in the table. Added buttons to index.html to allow user to select course and populate course data according to the course selected.

---
+++ js/main.js
@@ -0,0 +1,108 @@
+let coursePromise = fetch("https://golf-courses-api.herokuapp.com/courses/").then((response) => response.json());
+
+coursePromise.then((response) => printCourses(response));
+
+const courseSelectContainer = document.getElementById("course-select");
+
+function printCourses(data) {
+  let html = `
+        <h1>Courses</h1>
+        <p><button onclick="renderCourse(1)" id="course-1">Course 1</button> ${data.courses[0].name}</p>
+        <p><button onclick="renderCourse(2)" id="course-2">Course 2</button> ${data.courses[1].name}</p>
+        <p><button onclick="renderCourse(3)" id="course-3">Course 3</button> ${data.courses[2].name}</p>
+        `;
+  courseSelectContainer.innerHTML += html;
+}
+
+function renderCourse(course) {
+  if (course == 1) {
+    coursePromise
+      .then((response) => getCourse(response.courses[0].id)
+      ).then((response) => renderCourseData(response.data.holes));
+  }
+  if (course == 2) {
+    coursePromise
+      .then((response) => getCourse(response.courses[1].id)
+      ).then((response) => renderCourseData(response.data.holes));
+  }
+  if (course == 3) {
+    coursePromise
+      .then((response) => getCourse(response.courses[2].id)
+      ).then((response) => renderCourseData(response.data.holes));
+  }
+}
+
+function getCourse(course) {
+  return fetch(
+    `https://golf-courses-api.herokuapp.com/courses/${course}`
+  ).then((response) => response.json());
+}
+
+function renderCoursePar(holes) {
+  let outTotal = 0;
+  let inTotal = 0;
+  const parOutTotal = document.getElementById("par-out-total");
+  const parInTotal = document.getElementById("par-in-total");
+  for (i = 0; i < holes.length; i++) {
+    if (i < 9) {
+      const parElement = document.getElementById(`par-${i + 1}`);
+      parElement.innerText = holes[i].teeBoxes[0].par;
+      outTotal += holes[i].teeBoxes[0].par;
+    }
+    if (i > 8) {
+      const parElement = document.getElementById(`par-${i + 1}`);
+      parElement.innerText = holes[i].teeBoxes[0].par;
+      inTotal += holes[i].teeBoxes[0].par;
+    }
+  }
+  parOutTotal.innerText = outTotal;
+  parInTotal.innerText = inTotal;
+}
+
+function renderCourseYards(holes) {
+  let outTotal = 0;
+  let inTotal = 0;
+  const yardsOutTotal = document.getElementById("yards-out-total");
+  const yardsInTotal = document.getElementById("yards-in-total");
+  for (i = 0; i < holes.length; i++) {
+    if (i < 9) {
+      const yardsElement = document.getElementById(`yards-${i + 1}`);
+      yardsElement.innerText = holes[i].teeBoxes[0].yards;
+      outTotal += holes[i].teeBoxes[0].yards;
+    }
+    if (i > 8) {
+      const yardsElement = document.getElementById(`yards-${i + 1}`);
+      yardsElement.innerText = holes[i].teeBoxes[0].yards;
+      inTotal += holes[i].teeBoxes[0].par;
+    }
+  }
+  yardsOutTotal.innerText = outTotal;
+  yardsInTotal.innerText = inTotal;
+}
+
+function renderCourseHandicap(holes) {
+  let outTotal = 0;
+  let inTotal = 0;
+  const handicapOutTotal = document.getElementById("handicap-out-total");
+  const handicapInTotal = document.getElementById("handicap-in-total");
+  for (i = 0; i < holes.length; i++) {
+    if (i < 9) {
+      const handicapElement = document.getElementById(`handicap-${i + 1}`);
+      handicapElement.innerText = holes[i].teeBoxes[0].hcp;
+      outTotal += holes[i].teeBoxes[0].hcp;
+    }
+    if (i > 8) {
+      const handicapElement = document.getElementById(`handicap-${i + 1}`);
+      handicapElement.innerText = holes[i].teeBoxes[0].hcp;
+      inTotal += holes[i].teeBoxes[0].hcp;
+    }
+  }
+  handicapOutTotal.innerText = outTotal;
+  handicapInTotal.innerText = inTotal;
+}
+
+function renderCourseData(holes){
+    renderCoursePar(holes);
+    renderCourseYards(holes);
+    renderCourseHandicap(holes);
+}
