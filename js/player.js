b2438bd0f7a13480e67ebac78131b9e6a71073a1
SamNorman7103 <70028250+SamNorman7103@users.noreply.github.com>
2020-11-19 08:11:53 -0700

    Fixed player name validation for all player. Removed unused code

--- js/player.js
+++ js/player.js
@@ -5,13 +5,6 @@ export const playerOutContainer = document.getElementById(
 );
 export const playerInContainer = document.getElementById("player-in-container");
 export let playerCount = 0;
-export const players = [];
-export const playerNames = ["","","",""];
-
-export let player1rows;
-export let player2rows;
-export let player3rows;
-export let player4rows;
 
 export function renderPlayerData(count) {
   for (let player = 1; player <= count; player++) {
@@ -63,65 +56,91 @@ export function newPlayer() {
   playerOutIdElement.id = `player-${playerCount}`;
   playerInIdElement.id = `player-${playerCount}`;
 
-  const playerNameElement = playerOutContainer;
-
-  players.push([playerOutIdElement, playerInIdElement]);
-  if (playerCount == 1) {
-    player1rows = players[0];
-  }
-  if (playerCount == 2) {
-    player2rows = players[1];
-  }
-  if (playerCount == 3) {
-    player3rows = players[2];
-  }
-  if (playerCount == 4) {
-    player3rows = players[3];
-  }
   renderPlayerData(playerCount);
   $("input").keyup(function () {
     console.log("check");
     renderPlayerData(playerCount);
   });
 }
-function updatePlayerName(playerElement,str){
-    if (playerElement.id == "player-1"){
-        playerNames[0] = str;
-    }if (playerElement.id == "player-2"){
-      playerNames[1] = str;
-  }if (playerElement.id == "player-3"){
-      playerNames[2] = str;
-  }if (playerElement.id == "player-4"){
-      playerNames[3] = str;
+
+export function checkPlayerName(playerElement, name) {
+  if (playerElement.id == "player-1") {
+    for (let opponent = 1; opponent < playerCount; opponent++) {
+      if (opponent == 1) {
+        opponent++;
+      }
+      const opponentName = $(`#player-${opponent}`).find("textarea").val();
+      if (
+        name.length == opponentName.length &&
+        name.toLowerCase() == opponentName.toLowerCase()
+      ) {
+        name = "";
+        $("#player-1").find("textarea").val(name);
+        $("#player-1").find("textarea").attr("placeholder", "Name is taken");
+        setTimeout(function () {
+          $("#player-1").find("textarea").attr("placeholder", "");
+        }, 1750);
+      }
+    }
+  } else if (playerElement.id == "player-2") {
+    for (let opponent = 1; opponent < playerCount; opponent++) {
+      if (opponent == 2) {
+        opponent++;
+      }
+      const opponentName = $(`#player-${opponent}`).find("textarea").val();
+      if (
+        name.length == opponentName.length &&
+        name.toLowerCase() == opponentName.toLowerCase()
+      ) {
+        name = "";
+        $("#player-2").find("textarea").val(name);
+        $("#player-2").find("textarea").attr("placeholder", "Name is taken");
+        setTimeout(function () {
+          $("#player-2").find("textarea").attr("placaeholder", "");
+        });
+      }
+    }
+  } else if (playerElement.id == "player-3") {
+    for (let opponent = 1; opponent < playerCount; opponent++) {
+      if (opponent == 3) {
+        opponent++;
+      }
+      const opponentName = $(`#player-${opponent}`).find("textarea").val();
+      if (
+        name.length == opponentName.length &&
+        name.toLowerCase() == opponentName.toLowerCase()
+      ) {
+        name = "";
+        $("#player-3").find("textarea").val(name);
+        $("#player-3").find("textarea").attr("placeholder", "Name is taken");
+        setTimeout(function () {
+          $("#player-3").find("textarea").attr("placaeholder", "");
+        });
+      }
+    }
+  } else if (playerElement.id == "player-4") {
+    for (let opponent = 1; opponent < playerCount; opponent++) {
+      if (opponent == 4) {
+        break;
+      }
+      const opponentName = $(`#player-${opponent}`).find("textarea").val();
+      if (
+        name.length == opponentName.length &&
+        name.toLowerCase() == opponentName.toLowerCase()
+      ) {
+        name = "";
+        $("#player-4").find("textarea").val(name);
+        $("#player-4").find("textarea").attr("placeholder", "Name is taken");
+        setTimeout(function () {
+          $("#player-4").find("textarea").attr("placaeholder", "");
+        });
+      }
+    }
   }
-}
-function checkPlayerName(playerElement, name){
-    console.log(playerElement.id)
-    if (playerElement.id == "player-1") {
-        for (let opponent = 1; opponent <= playerCount; opponent++) {
-          if ((opponent == 1)) {
-            opponent++;
-          }
-          const opponentName = $(`#player-${opponent}`).find("textarea").val();
-          if (
-            name.length == opponentName.length &&
-            name.toLowerCase() == opponentName.toLowerCase()
-          ) {
-            name = "";
-            $("#player-1").find("textarea").val(name);
-            $("#player-1").find("textarea").attr("placeholder", "Name is taken");
-            setTimeout(function(){
-                $("#player-1").find("textarea").attr("placeholder", "")
-            }, 1750 )
-          }
-        }
-      } 
-    
 }
 
 playerOutContainer.addEventListener("keyup", (e) => {
   const currentPlayerNameElement = e.target.parentNode.parentNode;
   const name = e.target.value;
-  updatePlayerName(currentPlayerNameElement,name);
-  checkPlayerName(currentPlayerNameElement,name);
+  checkPlayerName(currentPlayerNameElement, name);
 });
